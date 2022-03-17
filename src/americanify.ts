b0e133bd4f357bd44900755eee07c152fea299be
willpoulson <wpoulson@crowdcomms.co.uk>
2022-03-17 16:18:12 +0000

    task(casing): ensure casing matches

--- src/americanify.ts
+++ src/americanify.ts
@@ -1,5 +1,24 @@
-import translations from './translations.json';
+import translations from "./translations.json";
 
 export function americanify(text: string) {
-    return text.split(' ').map((word) => translations[word] ?? word).join(' ');
+  return text
+    .split(" ")
+    .map((word) => {
+      const match = translations[word.toLowerCase()];
+
+      if (!match) {
+        return word;
+      }
+
+      return matchCasing(word, match);
+    })
+    .join(" ");
+}
+
+function matchCasing(original: string, translated: string): string {
+  if (original[0].toUpperCase() === original[0]) {
+    return `${translated[0].toUpperCase()}${translated.slice(1)}`;
+  }
+
+  return translated;
 }
